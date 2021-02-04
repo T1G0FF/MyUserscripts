fac4f79b670df20e604fabf18df91543b5c5c4bf
Thomas <Thomas.GitCommit@outlook.com>
2021-02-04 12:40:23 +1100

    Update BlankQuilting-StudioE.user.js

--- CollectionExtractors/BlankQuilting-StudioE.user.js
+++ CollectionExtractors/BlankQuilting-StudioE.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VicText Collection Extractor - Blank Quilting / Studio E
 // @namespace    http://www.tgoff.me/
-// @version      3.2.0
+// @version      3.2.1
 // @description  Gets the names and codes from a Blank Quilting or Studio E Collection
 // @author       www.tgoff.me
 // @match        *://www.blankquilting.net/*
@@ -154,20 +154,21 @@ function getItemObject(item) {
 	};
 }
 
-function formatInformation(item) {
-	let itemObject = getItemObject(item);
+function formatInformation(itemElement) {
+	let item = getItemObject(itemElement);
 
 	let tempCodeColour = (((item.ColourCode.length > 0) ? item.ColourCode + ' ' : '') + item.ColourName).toUpperCase();
 	let itemCode = formatItemCode(item.Prefix, item.CollectionCode + ' ' + tempCodeColour);
 	
 	let barCode = formatBarCode(itemCode);
 	
-	let description = formatSapDescription({ 'Colour': item.ColourName, 'Pattern': item.PatternName, 'Collection': (company === 'Studio E') ? getCompany() + ' ' + item.Collection : item.Collection, 'Special': item.SpecialNotes, 'Material': item.Material, 'Width': item.Width, 'Repeat': item.Repeat })
+	let widthString = item.Width.Measurement + item.Width.Unit;
+	let description = formatSapDescription({ 'Colour': item.ColourName, 'Pattern': item.PatternName, 'Collection': (company === 'Studio E') ? getCompany() + ' ' + item.Collection : item.Collection, 'Special': item.SpecialNotes, 'Material': item.Material, 'Width': widthString, 'Repeat': item.Repeat })
 
 	let webName = (((item.ColourName.length > 0) ? item.ColourName + ' - ' : '') + item.PatternName);
 
 	let relDateString = toReleaseString(item.ReleaseDates);
-	let webDesc = formatWebDescription({ 'Collection': item.Collection, 'Notes': item.SpecialNotes, 'Fibre': item.Material, 'Width': item.Width.Measurement + item.Width.Unit, 'Release': relDateString, 'Delivery From': dates.Delivery });
+	let webDesc = formatWebDescription({ 'Collection': item.Collection, 'Notes': item.SpecialNotes, 'Fibre': item.Material, 'Width': widthString, 'Release': relDateString, 'Delivery From': dates.Delivery });
 	let delDateString = toDeliveryString(item.ReleaseDates);
 
 	let result = { 'itemCode': itemCode, 'barCode': barCode, 'description': description, 'webName': webName, 'webDesc': webDesc, 'delDate': delDateString, 'purchaseCode': purchaseCode, 'webCategory': item.Collection };
