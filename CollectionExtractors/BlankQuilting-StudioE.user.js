2d4d091d9c3108a9c9bd16246f2959c39a5ea966
Thomas <Thomas.GitCommit@outlook.com>
2021-02-04 12:53:03 +1100

    Update BlankQuilting-StudioE.user.js

--- CollectionExtractors/BlankQuilting-StudioE.user.js
+++ CollectionExtractors/BlankQuilting-StudioE.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VicText Collection Extractor - Blank Quilting / Studio E
 // @namespace    http://www.tgoff.me/
-// @version      3.2.3
+// @version      3.2.4
 // @description  Gets the names and codes from a Blank Quilting or Studio E Collection
 // @author       www.tgoff.me
 // @match        *://www.blankquilting.net/*
@@ -170,10 +170,10 @@ function formatInformation(itemElement) {
 	let webName = (((item.ColourName.length > 0) ? item.ColourName + ' - ' : '') + item.PatternName);
 
 	let relDateString = toReleaseString(item.ReleaseDates);
-	let webDesc = formatWebDescription({ 'Collection': item.Collection, 'Notes': item.SpecialNotes, 'Fibre': item.Material, 'Width': widthString, 'Release': relDateString, 'Delivery From': dates.Delivery });
+	let webDesc = formatWebDescription({ 'Collection': item.Collection, 'Notes': item.SpecialNotes, 'Fibre': item.Material, 'Width': widthString, 'Release': relDateString, 'Delivery From': item.ReleaseDates.Delivery });
 	let delDateString = toDeliveryString(item.ReleaseDates);
 
-	let result = { 'itemCode': itemCode, 'barCode': barCode, 'description': description, 'webName': webName, 'webDesc': webDesc, 'delDate': delDateString, 'purchaseCode': purchaseCode, 'webCategory': item.Collection };
+	let result = { 'itemCode': itemCode, 'barCode': barCode, 'description': description, 'webName': webName, 'webDesc': webDesc, 'delDate': delDateString, 'purchaseCode': item.PurchaseCode, 'webCategory': item.Collection };
 	return result;
 
 	//////////////////////////////////////////////
