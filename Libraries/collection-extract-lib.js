55ef735d64958f0daf0faf78f1baf9235ac42303
Thomas <thomas.gitcommit@outlook.com>
2021-03-22 13:50:36 +1100

    Update collection-extract-lib.js
    Changed some z-indexes.

--- Libraries/collection-extract-lib.js
+++ Libraries/collection-extract-lib.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Collection Extraction Library
 // @namespace    http://www.tgoff.me/
-// @version      2021.03.18.1
+// @version      2021.03.22.1
 // @description  Implements the base functionality of downloading a Fabric Collection
 // @author       www.tgoff.me
 // @require      http://tgoff.me/tamper-monkey/tg-lib.js
@@ -375,6 +375,7 @@ function initDropdownContainer(element, direction = 'right') {
 	position: relative;
 	display: inline-flex;
 	margin: 0.5rem;
+	z-index: 999;
 }
 
 .tg-dropdown-button, .tg-dropdown-option {
