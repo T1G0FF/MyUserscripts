28c604aa6bc2c825c8b845ee6590f6b4f6b2d514
Thomas <thomas.gitcommit@outlook.com>
2021-07-01 17:02:30 +1000

    Update Timeless-DearStella.user.js
    Forgot to change version number.

--- CollectionExtractors/Timeless-DearStella.user.js
+++ CollectionExtractors/Timeless-DearStella.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VicText Collection Extractor - Dear Stella / Timeless Treasures
 // @namespace    http://www.tgoff.me/
-// @version      2021.04.12.1
+// @version      2021.07.01.1
 // @description  Gets the names and codes from a Dear Stella or Timeless Treasures Collection
 // @author       www.tgoff.me
 // @match        *://ttfabrics.com/category/*
