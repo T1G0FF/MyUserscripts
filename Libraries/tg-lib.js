df6ae413e57869c191b68b3730bb4fb58bc7a1d5
Thomas <thomas.gitcommit@outlook.com>
2021-10-08 11:15:04 +1100

    Update tg-lib.js
    Fixed a bug where it would not match the first/only word in a string.

--- Libraries/tg-lib.js
+++ Libraries/tg-lib.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TG Function Library
 // @namespace    http://www.tgoff.me/
-// @version      2021.10.08.1
+// @version      2021.10.08.2
 // @description  Contains various useful functions; includes CSS Style Manager, Toast notifications, a simple Queue, a Download Queue and URL Parameters.
 // @author       www.tgoff.me
 // ==/UserScript==
@@ -296,8 +296,8 @@ String.prototype.replaceAll = function(search, replacement = '', caseSensitive =
 
 String.prototype.toTitleCase = function(ignoreCase = true) {
 	let target = this;
-	// ([^.]) - The not dot is an attempt to ignore web addresses, it's far from perfect.
-	target = target.replace(/([^.])\b([A-z]+)\b/g, (match, p1, p2, offset, string) => {
+	// ([^.]?) - The not dot is an attempt to ignore web addresses, it's far from perfect.
+	target = target.replace(/([^.]?)\b([A-z]+)\b/g, (match, p1, p2, offset, string) => {
 		// Test for Roman Numerals
 		let romanNumeral = /(\b(?:[MDCLXVI])M*(?:C[MD]|D?C{0,3})(?:X[CL]|L?X{0,3})(?:I[XV]|V?I{0,3})\b)/.test(match);
 		let endOfWord = ignoreCase && !romanNumeral ? p2.slice(1).toLowerCase() : p2.slice(1);
