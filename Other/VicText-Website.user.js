3abd7648bf95d891a1deae273d127d5808939f96
Thomas <thomas.gitcommit@outlook.com>
2022-05-17 13:37:25 +1000

    Update VicText-Website.user.js
    Fixed another bug due to copy-pasted code.

--- Other/VicText-Website.user.js
+++ Other/VicText-Website.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VicText Website Additions
 // @namespace    http://www.tgoff.me/
-// @version      2022.05.17.3
+// @version      2022.05.17.4
 // @description  Adds Misc CSS, Item codes to swatch images, the option to show more items per page and a button to find items without images. Implements Toast popups.
 // @author       www.tgoff.me
 // @match        *://www.victoriantextiles.com.au/*
@@ -740,7 +740,7 @@ async function addHoverPreview() {
 	iFramePreview.onmouseout = function (event) {
 		if (event.relatedTarget === iFramePreview.caller
 			|| iFramePreview.caller.getAllChildren().includes(event.relatedTarget)) return;
-		hidePreview(iFramePreview);
+		MyiFrame.hide(iFramePreview);
 	};
 
 	let coll = await getCollection();
