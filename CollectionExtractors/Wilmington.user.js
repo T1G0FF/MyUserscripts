9e12ecc4c2a24d20a1895cc376bd7fb4f58685be
Thomas <thomas.gitcommit@outlook.com>
2022-07-19 13:24:51 +1000

    Update Wilmington.user.js
    Forgot to update version number.

--- CollectionExtractors/Wilmington.user.js
+++ CollectionExtractors/Wilmington.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VicText Collection Extractor - Wilmington
 // @namespace    http://www.tgoff.me/
-// @version      2022.07.19.5
+// @version      2022.07.19.6
 // @description  Gets the names and codes from a Wilmington Collection
 // @author       www.tgoff.me
 // @match        *://wilmingtonprints.com/*
