137eac404134155a7f9aea861755fa97eaac5d8e
Thomas <thomas.gitcommit@outlook.com>
2022-07-19 14:41:34 +1000

    Update collection-extract-lib.js
    Changed initDropdownContainer to now re-add the dropdown container if for some reason it is no longer in the DOM.

--- Libraries/collection-extract-lib.js
+++ Libraries/collection-extract-lib.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Collection Extraction Library
 // @namespace    http://www.tgoff.me/
-// @version      2022.06.07.5
+// @version      2022.07.19.1
 // @description  Implements the base functionality of downloading a Fabric Collection
 // @author       www.tgoff.me
 // @require      http://tgoff.me/tamper-monkey/tg-lib.js
@@ -568,9 +568,16 @@ function initDropdownContainer(locationElement, location = 'beforeEnd', directio
 		dropdownContainer.insertAdjacentElement('beforeEnd', dropdownMenu);
 		locationElement.insertAdjacentElement(location, dropdownContainer);
 
-		DROPDOWN_CONTAINERS[locationElement] = dropdownMenu;
+		DROPDOWN_CONTAINERS[locationElement].Container = dropdownContainer;
+		DROPDOWN_CONTAINERS[locationElement].Menu = dropdownMenu;
 	}
-	return DROPDOWN_CONTAINERS[locationElement];
+	else {
+		// Re-add dropdown container if it has been removed from the DOM
+		if (!document.querySelector('span.tg-dropdown-container')) {
+			locationElement.insertAdjacentElement(location, DROPDOWN_CONTAINERS[locationElement].Container);
+		}
+	}
+	return DROPDOWN_CONTAINERS[locationElement].Menu;
 }
 
 function addElementToDropdownContainer(locationElement, elementsToAdd, location = 'beforeEnd', showIf = true) {
