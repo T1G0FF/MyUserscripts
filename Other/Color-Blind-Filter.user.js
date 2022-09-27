81b940489b5f3042e0dc5ab5358b6aae9c0db71b
Thomas <thomas.gitcommit@outlook.com>
2022-09-27 13:17:40 +1000

    Update Color-Blind-Filter.user.js
    Changed overall direction to be a generic Accessibility Menu.
    Added functionality for a Reading Mask.

--- Other/Color-Blind-Filter.user.js
+++ Other/Color-Blind-Filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         General - Colour Blind Filter
+// @name         General - Accessibility Menu (Colour Blind Filter, ADHD Friendly Reading Mask)
 // @namespace    http://www.tgoff.me/
-// @version      2021.12.04.1
+// @version      2022.09.27.1
 // @description  Simulates the website as a color vision impaired person would see. Based on leocardz.com's Chrome Extension Colorblinding. Which is based on SVG data at https://github.com/Altreus/colourblind and Data matrices at http://web.archive.org/web/20081014161121/http://www.colorjack.com/labs/colormatrix/
 // @author       www.tgoff.me
 // @match        *://*/*
@@ -10,51 +10,86 @@
 // @run-at       document-idle
 // ==/UserScript==
 
-let simulations = {
-	'Red-Blind': 'protanopia',
-	'Green-Blind': 'deuteranopia',
-	'Blue-Blind': 'tritanopia',
-	'Red-Weak': 'protanomaly',
-	'Green-Weak': 'deuteranomaly',
-	'Blue-Weak': 'tritanomaly',
-	'Monochromacy': 'achromatopsia',
-	'Blue Cone Monochromacy': 'achromatomaly',
-	'None': 'none',
-	'Initial': 'init',
-};
+let colorBlindFilter = {
+	initialBackup: {},
+	simulations: {
+		'Red-Blind': 'protanopia',
+		'Green-Blind': 'deuteranopia',
+		'Blue-Blind': 'tritanopia',
+		'Red-Weak': 'protanomaly',
+		'Green-Weak': 'deuteranomaly',
+		'Blue-Weak': 'tritanomaly',
+		'Monochromacy': 'achromatopsia',
+		'Blue Cone Monochromacy': 'achromatomaly',
+		'None': 'none',
+		'Initial': 'init',
+	},
+	svgElement:
+		'<svg id="colorBlindSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" baseProfile="full">' +
+		'<filter id="protanopia">' +
+		'<feColorMatrix type="matrix" values="0.567, 0.433, 0, 0, 0, 0.558, 0.442, 0, 0, 0, 0, 0.242, 0.758, 0, 0, 0, 0, 0, 1, 0" in="SourceGraphic"></feColorMatrix>' +
+		'</filter>' +
+		'<filter id="protanomaly">' +
+		'<feColorMatrix type="matrix" values="0.817,0.183,0,0,0 0.333,0.667,0,0,0 0,0.125,0.875,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
+		'</filter>' +
+		'<filter id="deuteranopia">' +
+		'<feColorMatrix type="matrix" values="0.625,0.375,0,0,0 0.7,0.3,0,0,0 0,0.3,0.7,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
+		'</filter>' +
+		'<filter id="deuteranomaly">' +
+		'<feColorMatrix type="matrix" values="0.8,0.2,0,0,0 0.258,0.742,0,0,0 0,0.142,0.858,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
+		'</filter>' +
+		'<filter id="tritanopia">' +
+		'<feColorMatrix type="matrix" values="0.95,0.05,0,0,0 0,0.433,0.567,0,0 0,0.475,0.525,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
+		'</filter>' +
+		'<filter id="tritanomaly">' +
+		'<feColorMatrix type="matrix" values="0.967,0.033,0,0,0 0,0.733,0.267,0,0 0,0.183,0.817,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
+		'</filter>' +
+		'<filter id="achromatopsia">' +
+		'<feColorMatrix type="matrix" values="0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
+		'</filter>' +
+		'<filter id="achromatomaly">' +
+		'<feColorMatrix type="matrix" values="0.618,0.320,0.062,0,0 0.163,0.775,0.062,0,0 0.163,0.320,0.516,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
+		'</filter>' +
+		'<filter id="none">' +
+		'</filter>' +
+		'</svg>',
 
-let filterElement =
-	'<svg id="colorBlindSVG" version="1.1" xmlns="http://www.w3.org/2000/svg" baseProfile="full">' +
-	'<filter id="protanopia">' +
-	'<feColorMatrix type="matrix" values="0.567, 0.433, 0, 0, 0, 0.558, 0.442, 0, 0, 0, 0, 0.242, 0.758, 0, 0, 0, 0, 0, 1, 0" in="SourceGraphic"></feColorMatrix>' +
-	'</filter>' +
-	'<filter id="protanomaly">' +
-	'<feColorMatrix type="matrix" values="0.817,0.183,0,0,0 0.333,0.667,0,0,0 0,0.125,0.875,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
-	'</filter>' +
-	'<filter id="deuteranopia">' +
-	'<feColorMatrix type="matrix" values="0.625,0.375,0,0,0 0.7,0.3,0,0,0 0,0.3,0.7,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
-	'</filter>' +
-	'<filter id="deuteranomaly">' +
-	'<feColorMatrix type="matrix" values="0.8,0.2,0,0,0 0.258,0.742,0,0,0 0,0.142,0.858,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
-	'</filter>' +
-	'<filter id="tritanopia">' +
-	'<feColorMatrix type="matrix" values="0.95,0.05,0,0,0 0,0.433,0.567,0,0 0,0.475,0.525,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
-	'</filter>' +
-	'<filter id="tritanomaly">' +
-	'<feColorMatrix type="matrix" values="0.967,0.033,0,0,0 0,0.733,0.267,0,0 0,0.183,0.817,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
-	'</filter>' +
-	'<filter id="achromatopsia">' +
-	'<feColorMatrix type="matrix" values="0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0.299,0.587,0.114,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
-	'</filter>' +
-	'<filter id="achromatomaly">' +
-	'<feColorMatrix type="matrix" values="0.618,0.320,0.062,0,0 0.163,0.775,0.062,0,0 0.163,0.320,0.516,0,0 0,0,0,1,0" in="SourceGraphic"></feColorMatrix>' +
-	'</filter>' +
-	'<filter id="none">' +
-	'</filter>' +
-	'</svg>';
+}
+
+let readingMask = {
+	topElement: {},
+	bottomElement: {},
+	create: function() {
+			if (document.querySelectorAll(".ReadingMaskTop").length) return;
+
+			document.body.classList.add("ReadingMask");
+			readingMask.topElement = document.createElement("div");
+			readingMask.topElement.classList.add("ReadingMaskTop");
+			document.body.appendChild(readingMask.topElement);
+			readingMask.bottomElement = document.createElement("div");
+			readingMask.bottomElement.classList.add("ReadingMaskBottom");
+			document.body.appendChild(readingMask.bottomElement);
+			document.addEventListener("mousemove", readingMask.move);
+			document.addEventListener("click", readingMask.move);
+		},
+	move: function(event) {
+			let mouseY = event.clientY;
+			let readingMaskHeight = (window.outerHeight + window.innerHeight) * 0.075;
+			let halfHeight = Math.round(readingMaskHeight / 2);
+			readingMask.topElement.style.height = mouseY - halfHeight + "px";
+			readingMask.bottomElement.style.top = mouseY + halfHeight + "px";
+		},
+	remove: function() {
+			document.body.classList.remove("ReadingMask");
+			readingMask.topElement.remove();
+			readingMask.bottomElement.remove();
+			document.removeEventListener("mousemove", readingMask.move);
+			document.removeEventListener("click", readingMask.move);
+		}
+}
 
 let cssText = `
-#ColorBlindToggleContainer {
+#AccessibilityToggleContainer {
 	position: fixed;
 	background: none;
 	bottom: 1px;
@@ -64,7 +99,7 @@ let cssText = `
 	z-index: 2147483647;
 }
 
-.ColorBlindToggleLabel {
+.AccessibilityToggleLabel {
 	display: block;
 	text-align: right;
 	color: #F2F2F2;
@@ -75,48 +110,58 @@ let cssText = `
 	text-shadow: 1px 1px black, -1px -1px black, -1px 1px black, 1px -1px black;
 }
 
-input[id*="ColorBlindToggleRadioButton"] {
+#AccessibilityToggleContainer input {
 	float: none;
 	margin-left: 10px;
 }
 
-#ColorBlindToggleContainer .ColorBlindToggleLabel {
+#AccessibilityToggleContainer .AccessibilityToggleLabel {
 	display: none;
 	visibility: hidden;
 }
 
-#ColorBlindToggleContainer:hover {
+#AccessibilityToggleContainer:hover {
 	height: unset;
 	width: unset;
 }
 
-#ColorBlindToggleContainer:hover .ColorBlindToggleLabel {
+#AccessibilityToggleContainer:hover .AccessibilityToggleLabel {
 	display: block;
 	visibility: visible;
 }
+
+body.ReadingMask .ReadingMaskTop,
+body.ReadingMask .ReadingMaskBottom {
+	display: block;
+	position: fixed;
+	left: 0;
+	right: 0;
+	width: 100%;
+	background-color: rgba(0, 0, 0, 0.7);
+	z-index: 999999;
+}
+
+body.ReadingMask .ReadingMaskTop {
+	top: 0;
+	bottom: auto;
+}
+
+body.ReadingMask .ReadingMaskBottom {
+	bottom: 0;
+	top: auto;
+}
 `;
 
-let filterBackup;
 (function () {
 	'use strict';
 	setTimeout(function () {
 		let container = document.createElement('div');
-		container.id = 'ColorBlindToggleContainer';
+		container.id = 'AccessibilityToggleContainer';
 		document.body.appendChild(container);
 
-		let filterDiv = document.createElement('div');
-		filterDiv.id = 'colorBlindFilters';
-		filterDiv.style.width = '0px';
-		filterDiv.style.height = '0px';
-		filterDiv.style.display = 'none';
-		filterDiv.innerHTML = filterElement;
-		container.appendChild(filterDiv);
-
 		_addStyle(cssText);
-		for (const name in simulations) {
-			let id = simulations[name];
-			let radio = _addColorBlindToggle(container, name, id);
-		}
+		_addReadingMaskToggle(container);
+		_addColorBlindToggles(container);
 	}, 500);
 })();
 
@@ -133,6 +178,21 @@ function _addStyle(css) {
 	return node;
 };
 
+function _addColorBlindToggles(container) {
+	let filterDiv = document.createElement('div');
+	filterDiv.id = 'colorBlindFilters';
+	filterDiv.style.width = '0px';
+	filterDiv.style.height = '0px';
+	filterDiv.style.display = 'none';
+	filterDiv.innerHTML = colorBlindFilter.svgElement;
+	container.appendChild(filterDiv);
+
+	for (const name in colorBlindFilter.simulations) {
+		let id = colorBlindFilter.simulations[name];
+		let radio = _addColorBlindToggle(container, name, id);
+	}
+}
+
 function _addColorBlindToggle(container, name, id) {
 	let nameKey = id.toUpperCase();
 
@@ -144,10 +204,10 @@ function _addColorBlindToggle(container, name, id) {
 
 	if (id === 'init') {
 		let htmlElement = document.getElementsByTagName('html')[0];
-		filterBackup = htmlElement.style.filter;
+		colorBlindFilter.initialBackup = htmlElement.style.filter;
 		radio.onclick = function () {
 			let htmlElement = document.getElementsByTagName('html')[0];
-			htmlElement.style.filter = filterBackup;
+			htmlElement.style.filter = colorBlindFilter.initialBackup;
 		};
 		radio.setAttribute("checked", "checked");
 		radio.checked = true;
@@ -162,9 +222,33 @@ function _addColorBlindToggle(container, name, id) {
 	let label = document.createElement('label');
 	label.setAttribute('for', thisId);
 	label.innerText = name;
-	label.classList.add('ColorBlindToggleLabel');
+	label.classList.add('AccessibilityToggleLabel');
 
 	label.appendChild(radio);
 	container.appendChild(label);
 	return radio;
+};
+
+function _addReadingMaskToggle(container) {
+	let check = document.createElement('input');
+	check.type = 'checkbox';
+	check.name = 'ReadingMask';
+	let thisId = 'ReadingMaskToggleCheckBox';
+	check.id = thisId;
+
+	check.setAttribute("checked", "unchecked");
+	check.checked = false;
+
+	check.onclick = function () {
+		check.checked ? readingMask.create() : readingMask.remove();
+	}
+
+	let label = document.createElement('label');
+	label.setAttribute('for', thisId);
+	label.innerText = 'Reading Mask';
+	label.classList.add('AccessibilityToggleLabel');
+
+	label.appendChild(check);
+	container.appendChild(label);
+	return check;
 };
