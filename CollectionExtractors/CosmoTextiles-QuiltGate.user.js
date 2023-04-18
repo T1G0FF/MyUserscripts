7a22c612cd8b8b7cfcc2b2cfb36ae431d0830116
Thomas <thomas.gitcommit@outlook.com>
2023-04-18 16:40:46 +1000

    Update CosmoTextiles-QuiltGate.user.js
    Added code that scrapes from cosmo-tex.co.jp as well.

--- CollectionExtractors/CosmoTextiles-QuiltGate.user.js
+++ CollectionExtractors/CosmoTextiles-QuiltGate.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         VicText Collection Extractor - Cosmo Textiles / Quilt Gate
 // @namespace    http://www.tgoff.me/
-// @version      2023.04.18.2
+// @version      2023.04.18.3
 // @description  Gets the names and codes from a Cosmo Textiles / Quilt Gate Collection
 // @author       www.tgoff.me
 // @match        *://www.quilt-gate.com/eng/detail.php?*
+// @match        *://cosmo-tex.co.jp/hcd/search_detail.php?*
 // @require      https://raw.githubusercontent.com/T1G0FF/MyUserscripts/main/Libraries/tg-lib.js
 // @require      https://raw.githubusercontent.com/T1G0FF/MyUserscripts/main/Libraries/collection-extract-lib.js
 // @grant        GM_setClipboard
@@ -12,8 +13,10 @@
 // @run-at       document-idle
 // ==/UserScript==
 
+let isCosmo = false;
 (function () {
 	'use strict';
+	isCosmo = window.location.hostname.includes('cosmo-tex');
 	createButtons();
 })();
 
@@ -23,7 +26,7 @@ function getCompany() {
 }
 
 function getTitleElement() {
-	let titleElement = document.querySelector('div#mainContent table td h3')
+	let titleElement = isCosmo ? document.querySelector('div#main_content_box h2 span:last-of-type') : document.querySelector('div#mainContent table td h3');
 	return titleElement;
 }
 
@@ -31,12 +34,26 @@ function getTitle() {
 	let titleElement = getTitleElement();
 	let title = titleElement?.innerText.trim();
 	title = title.replace(titleElement.querySelector('span.tg-dropdown-container')?.innerText, '').trim();
-	if (title.indexOf(' - ') > 0) title = title.split(' - ')[0];
-	return title;
+
+	if (isCosmo) {
+		var tempTitle = document.querySelector('div#main_content_box h2 span strong')?.innerText;
+		if (tempTitle.indexOf('【品番】') >= 0) tempTitle = tempTitle.split('【品番】')[1];
+		tempTitle = tempTitle.trim();
+
+		if (title.indexOf('（') >= 0) title = title.split('（')[1];
+		if (title.indexOf('）') >= 0) title = title.split('）')[0];
+		if (title.indexOf('商品名 ：') >= 0) title = title.split('商品名 ：')[1];
+		title = title.replace(tempTitle, '');
+	}
+	else {
+		if (title.indexOf(' - ') >= 0) title = title.split(' - ')[0];
+	}
+	
+	return title.trim();
 }
 
 function getCollection() {
-	let collection = document.querySelectorAll('div#mainContent table div.product_box_subimage');
+	let collection = isCosmo ? document.querySelectorAll('div#main_content_box table a[data-lightbox="color_img"]') : document.querySelectorAll('div#mainContent table div.product_box_subimage a');
 	return collection;
 }
 
@@ -45,7 +62,7 @@ function getAvailabilityDate() {
 	return undefined;
 }
 
-let CosmoRegEx = /([A-z]+[0-9]+)_([0-9]+)([A-z]+)/;
+let CosmoRegEx = /([A-z]+[0-9]+)(?:_([0-9]+))?([A-z]+)/;
 let RegexEnum = {
 	'Collection': 1,
 	'Pattern': 2,
@@ -53,13 +70,13 @@ let RegexEnum = {
 };
 
 function getItemObject(item) {
-	let imgLink = item.querySelector('a')?.getAttribute('href');
+	let imgLink = item.getAttribute('href');
 	if (!imgLink) {
 		Notify.log('No image link found for Item!', item);
 		return;
 	}
 
-	let codeMatches = /products_photo\/(.*?)\.jpg/.exec(imgLink);
+	let codeMatches = isCosmo ? /shohin_images\/hcd_big\/[A-z0-9]+\/(.*?)\.jpg/.exec(imgLink) : /products_photo\/(.*?)\.jpg/.exec(imgLink);
 	if (!codeMatches || codeMatches.length <= 1) {
 		Notify.log('No matches found in link for Item!', item);
 		return;
@@ -77,10 +94,10 @@ function getItemObject(item) {
 	}
 
 	let collectionCode = matches[RegexEnum.Collection];
-	let patternCode = padWithZeros(matches[RegexEnum.Pattern], 3);
+	let patternCode = matches[RegexEnum.Pattern] ? padWithZeros(matches[RegexEnum.Pattern], 3) : '';
 	let colourCode = matches[RegexEnum.Colour].toUpperCase();
 
-	let purchaseCode = formatPurchaseCode(matches[RegexEnum.Collection] + '-' + matches[RegexEnum.Pattern] + matches[RegexEnum.Colour]);
+	let purchaseCode = formatPurchaseCode(matches[RegexEnum.Collection] + '-' + (matches[RegexEnum.Pattern] ?? '') + matches[RegexEnum.Colour]);
 
 	let title = getFormattedTitle();
 	let special = '';
