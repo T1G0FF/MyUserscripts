ca086868eb8901b1c800e1fbdacfcf688ad91e2e
Thomas <thomas.gitcommit@outlook.com>
2023-10-13 11:45:01 +1100

    Update Madeira.user.js
    Updated to actually parse out the information.

--- CollectionExtractors/Madeira.user.js
+++ CollectionExtractors/Madeira.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VicText Collection Extractor - Madeira
 // @namespace    http://www.tgoff.me/
-// @version      2023.10.13.1
+// @version      2023.10.13.2
 // @description  Gets the names and codes from a Madeira Range
 // @author       www.tgoff.me
 // @match        *://www.madeirausa.com/*
@@ -25,7 +25,7 @@
 
 		var link = document.createElement('a');
 		link.innerHTML = itemCountElem.outerHTML;
-		link.setAttribute('href', window.location.origin + window.location.pathname + '?cp=1&ppp=' + itemCountElem.innerText + '&sort=featured');
+		link.setAttribute('href', window.location.origin + window.location.pathname + '?cp=1&ppp=' + itemCountElem.innerText + '&sort=title');
 
 		itemCountElem.parentNode.insertBefore(link, itemCountElem);
 		itemCountElem.remove();
@@ -51,42 +51,58 @@ function formatInformation(item) {
 	let title = getFormattedTitle();
 	let company = getCompany();
 
-	let givenName = item.querySelector('a.productTitle');
-	let firstSpace = givenName.indexOf(' ');
-	let givenCode = givenName.substr(0, firstSpace);
-	let givenTitle = givenName.substr(firstSpace + 1);
+	let nameElem = item.querySelector('a.productTitle');
+	let givenName = nameElem?.innerText ?? '';	// 910-1001 MADEIRA CLASSIC RAYON #40 WEIGHT
+	let spaceIndex = givenName.indexOf(' ');
+	let givenCode = givenName.substr(0, spaceIndex); // 910-1001
+	let givenTitle = givenName.substr(spaceIndex + 1); // MADEIRA CLASSIC RAYON #40 WEIGHT
 	let hashIndex = givenTitle.indexOf('#');
 	let givenWeight = '';
 	if (hashIndex >= 0) {
-		givenWeight = givenTitle.substr(hashIndex);
-		givenTitle = givenTitle.substr(0, hashIndex).trim();
+		givenWeight = givenTitle.substr(hashIndex); // #40 WEIGHT
+		givenTitle = givenTitle.substr(0, hashIndex).trim(); // MADEIRA CLASSIC RAYON
 	}
 
-	let givenDesc = item.querySelector('div.shortDesc');
+	let descElem = item.querySelector('div.shortDesc');	
+	let givenDesc = descElem?.innerText ?? ''; // 5500yd CONE WHITE
 	let givenSize = '';
 	let givenColour = '';
-
-	for (const size in ['CONE', 'SPOOL']) {
-		let index = givenDesc.indexOf(size);
-		if (index >= 0) {
-			index += size.length;
-			givenSize = givenDesc.substr(0, index);
-			givenColour = givenDesc.substr(index);
+	for (const size of ['CONE', 'SPOOL']) {
+		let sizeIndex = givenDesc.indexOf(size);
+		if (sizeIndex >= 0) {
+			sizeIndex += size.length;
+			givenSize = givenDesc.substr(0, sizeIndex);	// 5500yd CONE
+			givenColour = givenDesc.substr(sizeIndex).trim();	// WHITE
 			break;
 		}
 	}
 
-	debugger;
-
-	let itemCode = (givenName + ' ' + colorCode).toUpperCase();
+	let prefix = '';
+	let itemCode = (prefix + givenCode).toUpperCase();
 	let barCode = formatBarCode(itemCode);
-	let purchaseCode = itemCode;
-	let material = document.querySelector('div.product-detail__header > p').innerText.trim();
-	let length = getLength();
+	let purchaseCode = givenCode.toUpperCase();
 
-	let webName = givenColour.toTitleCase() + ' - ' + title;
-	let webDesc = material + ' - ' + length;
-	let description = webName + ' - ' + webDesc;
+	let colour = givenColour.toTitleCase();
+	let name = givenTitle.replace('MADEIRA', '').trim().toTitleCase();
+	let length = getLengths(givenSize);
+	let material = 'MAT%';
+	let weight = givenWeight.replace(/#([0-9]+) WEIGHT/, '$1W');
+	
+	// Metrosene 100 150m - 4000 Black
+	let mesStr = length.hasOwnProperty('Metres') ? `${length.Metres.Measurement}${length.Metres.Unit}` : `${length.Measurement}${length.Unit}`;
+	let webName = `${name} ${weight} - ${mesStr} - ${givenCode} ${colour}`;
+	// Black - Metrosene 100 - 150m (164yd) - P100% - 60W/2P
+	let description = `${colour} - ${name} ${weight} - ${getLengthString(length)} - ${material} - ${weight}`;
+	/*
+	!! ONLY APPEARS ON PARENT !!
+	Cotton, synthetics, mixed fabrics, linen or silk. Clothes make the man. And the universal thread METROSENE will ensure that you and your clothes make a particularly dazzling appearance - and more. Its excellent smoothness, high tensile strength and ideal sewability make the universal METROSENE a reliable partner for all your creations.<br>
+	<b>Fibre: </b>100% Polyester, Corespun<br>
+	<b>Length: </b>150m|164yd<br>
+	<b>Weight: </b>60W/2Ply<br>
+	<b>Box of: </b>5<br>
+	<b>Needle size: </b>80-90
+	*/
+	let webDesc = '';
 
 	let delDate = '';//toDeliveryString(getReleaseDates());
 	let result = { 'itemCode': itemCode, 'barCode': barCode, 'description': description, 'webName': webName, 'webDesc': webDesc, 'delDate': delDate, 'purchaseCode': purchaseCode };
@@ -102,4 +118,40 @@ function formatImage(item) {
 
 	let fullUrl = thumbUrl.replace('cache/', '').replace('_375x387-pad', '');
 	return fullUrl;
+}
+
+function getLengths(lengthInput) {
+	for (const size of ['CONE', 'SPOOL']) {
+		lengthInput = lengthInput.replace(size, '').trim();
+	}
+
+	let measurement = parseInt(lengthInput);
+	let metres = -1;
+	let yards = -1;
+	let unit = lengthInput.replace(/^[0-9]+/, '');
+	switch (unit) {
+		case 'yd':
+			yards = measurement;
+			metres = yards * 0.9144;
+			metres = MRound(metres, 5);
+			break;
+		case 'm':
+			metres = measurement;
+			yards = metres / 0.9144;
+			yards = MRound(yards, 5);
+		default:
+			return { 'Measurement': measurement, 'Unit': unit };
+	}
+	return {
+		'Metres': { 'Measurement': metres, 'Unit': 'm' },
+		'Yards': { 'Measurement': yards, 'Unit': 'yd' }
+	};
+}
+
+function getLengthString(lengthObject) {
+	return lengthObject.hasOwnProperty('Metres') ? `${lengthObject.Metres.Measurement}m (${lengthObject.Yards.Measurement}yd)` : `${lengthObject.Measurement}${lengthObject.Unit}`;
+}
+
+function MRound(number, roundto) {
+	return roundto * Math.round(number / roundto);
 }
