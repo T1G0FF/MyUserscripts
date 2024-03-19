fb3d6adfa97d671252e19a05865bafab1ac8d386
Thomas <thomas.gitcommit@outlook.com>
2024-03-19 15:24:15 +1100

    Update Save-Image-As-Type.user.js
    Updated to remove events on success.

--- Other/Save-Image-As-Type.user.js
+++ Other/Save-Image-As-Type.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         General - Save Image as Type
 // @namespace    http://www.tgoff.me/
-// @version      2023.07.14.1
+// @version      2024.03.07.1
 // @description  Based on 'Save image as Type' Chrome extension by 'html5gamer' (https://chrome.google.com/webstore/detail/save-image-as-type/gabfmnliflodkdafenbcpjdlppllnemd)
 // @author       www.tgoff.me
 // @match        *://*/*
-// @grant        GM_registerMenuCommand
 // @run-at       document-start
 // @noframes
 // ==/UserScript==
@@ -24,45 +23,58 @@ function rightClickAction(event) {
 		if (event.ctrlKey && event.button === 2) {
 			event.preventDefault();
 			// If also holding shift save as jpg
-			target.addEventListener("load", event.shiftKey ? _saveAsJPEG : _saveAsPNG); 
+			target.addEventListener('load', event.shiftKey ? _saveAsJPEG : _saveAsPNG);
 			target.src = target.src; // Force Onload event to reoccur.
 		}
 	}
 }
 
-function _saveAsPNG(event) {
+async function _saveAsPNG(event) {
 	let img = event.target;
-	saveAsType(img, 'png');
+	await saveAsType(img, 'png');
+	img.removeEventListener('load', _saveAsPNG);
 }
 
-function _saveAsJPEG(event) {
+async function _saveAsJPEG(event) {
 	let img = event.target;
-	saveAsType(img, 'jpeg');
+	await saveAsType(img, 'jpeg');
+	img.removeEventListener('load', _saveAsJPEG);
 }
 
-function saveAsType(img, type) {
+async function saveAsType(img, type) {
 	img.crossOrigin = 'anonymous';
+	let mimeType = 'image/' + (type == 'jpg' ? 'jpeg' : type);
+	let fileName = getSuggestedFilename(img.src, type);
+	let noChange = img.src.startsWith('data:' + mimeType + ';');
+	if (noChange) {
+		downloadUrl(img.src, fileName);
+		return;
+	}
+
 	if (!canvas) {
 		canvas = document.createElement('canvas');
 	}
 	canvas.width = img.naturalWidth;
 	canvas.height = img.naturalHeight;
-	var context = canvas.getContext('2d');
-	var mimeType = 'image/' + (type == 'jpg' ? 'jpeg' : type);
-	context.drawImage(img, 0, 0);
-	var dataurl = canvas.toDataURL(mimeType);
-	var fileName = getSuggestedFilename(img.src, type);
-	downloadUrl(dataurl, fileName)
+	let context = canvas.getContext('2d');
+	await context.drawImage(img, 0, 0);
+	let dataUrl = canvas.toDataURL(mimeType);
+
+	downloadUrl(dataUrl, fileName);
 }
 
 function downloadUrl(url, fileName) {
 	if (!downloadLink) {
 		downloadLink = document.createElement('a');
-		downloadLink.style.display = "None";
+		downloadLink.setAttribute('target', '_blank');
+		downloadLink.style.display = 'None';
 	}
-	downloadLink.href = url;
 	downloadLink.setAttribute('download', fileName);
+	downloadLink.href = url;
+
+	document.body.appendChild(downloadLink);
 	downloadLink.click();
+	downloadLink.remove();
 }
 
 function getSuggestedFilename(src, type) {
@@ -70,20 +82,23 @@ function getSuggestedFilename(src, type) {
 	if (src.match(/googleusercontent\.com\/[0-9a-zA-Z]{30,}/)) {
 		return 'screenshot.' + type;
 	}
-	var fileName = src.replace(/[?#].*/, '').replace(/.*[\/]/, '').replace(/\+/g, ' ');
+	if (src.startsWith('blob:') || src.startsWith('data:')) {
+		return 'Untitled.' + type;
+	}
+	let fileName = src.replace(/[?#].*/, '').replace(/.*[\/]/, '').replace(/\+/g, ' ');
 	fileName = decodeURIComponent(fileName);
 	fileName = fileName.replace(/[\x00-\x7f]+/g, function (s) { // Remove Unicode characters?
 		return s.replace(/[^\w\-\.\,@ ]+/g, '');
 	});
-	while (fileName.match(/\.[^0-9a-zA-Z]*\./)) {
-		fileName = fileName.replace(/\.[^0-9a-zA-Z]*\./g, '.'); // Remove words between dots?
+	while (fileName.match(/\.[^0-9a-z]*\./)) {
+		fileName = fileName.replace(/\.[^0-9a-z]*\./g, '.'); // Remove words between dots?
 	}
-	fileName = fileName.replace(/\s{2,}/g, ' ').trim(); // Remove multiple whitespace
+	fileName = fileName.replace(/\s\s+/g, ' ').trim(); // Remove multiple whitespace
 	fileName = fileName.replace(/\.(jpe?g|png|gif|webp|svg)$/gi, '').trim(); // Remove extension
 	if (fileName.length > 32) {
 		fileName = fileName.substr(0, 32);
 	}
-	fileName = fileName.replace(/[^0-9a-zA-Z]+$/, '').trim(); //Trailing non-word code
+	fileName = fileName.replace(/[^0-9a-z]+$/i, '').trim(); //Trailing non-word code
 	if (!fileName) {
 		fileName = 'image';
 	}
