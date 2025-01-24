86c73ab32514d7f28fa9963b9fbb765b35368c33
Thomas <thomas.gitcommit@outlook.com>
2025-01-24 16:11:32 +1100

    Update MarshallDryGoods.user.js
    Fairly close to being functional

--- CollectionExtractors/MarshallDryGoods.user.js
+++ CollectionExtractors/MarshallDryGoods.user.js
@@ -1,17 +1,27 @@
 // ==UserScript==
 // @name         VicText Collection Extractor - Marshall Dry Goods
 // @namespace    http://www.tgoff.me/
-// @version      2024.12.06.1
+// @version      2025.01.24.1
 // @description  Gets the names and codes from a Marshall Dry Goods Collection
 // @author       www.tgoff.me
 // @match        *://www.marshalldrygoods.com/shop/fabrics/*
 // @match        *://marshalldrygoods.com/shop/fabrics/*
+// @match        *://www.marshalldrygoods.com/shop/wholesale/*
+// @match        *://marshalldrygoods.com/shop/wholesale/*
+// @match        *://www.marshalldrygoods.com/shop/home/search/results/*
+// @match        *://marshalldrygoods.com/shop/home/search/results/*
 // @require      https://raw.githubusercontent.com/T1G0FF/MyUserscripts/main/Libraries/tg-lib.js
 // @require      https://raw.githubusercontent.com/T1G0FF/MyUserscripts/main/Libraries/collection-extract-lib.js
 // @grant        GM_setClipboard
 // @grant        GM_download
 // @run-at       document-idle
 // ==/UserScript==
+
+var CollectionList =
+	['108 BRUSH STROKES', '108 CONFETTI DIGITAL', '108 DANCIN DAHLIAS', '108 DIGITALLY PRINTED BATIK PATTERN', '108 DREAM COTTON SOLIDS', '108 EARTH JEWELS', '108 FAIRY TALE', '108 FLANNEL PRINTS', '108 KALEIDOSCOPE DIGITAL', '108 CLASSICS', '108 PRINTED QUILT BACKING', '108 QUEENS GARDEN', '108 QUILTERS BLENDERS', '108 SCROLLING TONAL', '108 TONE ON TONE', '108 MARBLEICIOUS', '108 DAY DREAM', '108 TILT-A-WHIRL', '108 SCRAMBLE TONAL', '108 SMOOTHIE', '108 MARBLE TONAL', '108 BLENDED NATURAL', '108 GRUNGE PAINT', '108 CIRCLE BACK', '108 BLENDED', '108 PATRIOTIC', '108 PAISLEY', '108 TONAL VINEYARD', '108 BRANCHES', '108 FADED FLORAL',
+		'45 BRUSH STROKES', '45 CONFETTI DIGITAL', '45 DANCIN DAHLIAS', '45 DIGITALLY PRINTED BATIK PATTERN', '45 DREAM COTTON', '45 EARTH JEWELS', '45 FAIRY TALE', '45 GEMSTONES', '45 KALEIDOSCOPE', '45 VINTAGE FLORALS', '45 PALETTE PATCH', '45 QUEENS GARDEN', '45 SCROLLING TONAL', '45 VINE DANCE', '45 WATERCOLOR DIGITAL',
+		'60 PRIME PLUS POLY COTTON SOLIDS', '60 PRINTS', '90 QUILT TOP', '90 SHEETING',
+		'BABY PANELS', 'BABY PANELS PRE QUILTED', 'CHRISTMAS PRINTS', 'CLOSEOUTS', 'COLLEGE PRINTS', 'CRACKED ICE', 'CUDDLE FLEECE', 'DIGITAL NOVELTY', 'DUCK CANVAS', 'FLANNEL PRINTS', 'HOMESPUN CHECKS', 'INDONESIAN HAND STAMPED BATIKS', 'KNITS', 'LICENSED', 'MARSHALL DRY GOODS CLASSICS', 'BASICS', 'CAMOUFLAGE', 'DIGITAL NOVELTY', 'DIGITAL PRINTS', 'JELLY ROLLS', 'PRE PACKAGED FABRIC', 'MLB BASEBALL', 'MUSLIN', 'NBA', 'NFL PRINTS', 'PLUSH FABRICS', 'PREMIUM HAND STAMPED BATIKS', 'PROMOTIONAL SOLIDS', 'QUILTERS BATIKS', 'QUILTERS BLENDERS', 'QUILTERS CALICOS', 'QUILTERS TIE DYES', 'ROCKY POLY COTTON POPLIN', 'SLINKY MINKY', 'SLINKY MINKY DOTS', 'SLINKY MINKY SOLIDS', 'SNUGGLE FLANNEL', 'SPLATTER TONAL', 'SPORTS TEAMS', 'THE RED THE BLACK', 'TONAL BRANCHES', 'TONE ON TONE', 'VINTAGE 1800S', 'WIDE POLY COTTON SHEETING'];
 
 (function () {
 	'use strict';
@@ -32,7 +42,6 @@ function getCollection() {
 	return document.querySelectorAll('#cartHome div.products > div.item');
 }
 
-
 function getItemObject(item) {
 	let codeElement = item.querySelector('a.mt-3 strong');
 	if (!codeElement) {
@@ -41,19 +50,29 @@ function getItemObject(item) {
 	}
 
 	let givenCode = codeElement.innerText.trim().toUpperCase();
-	if (givenCode.indexOf('3 YARD PIECE') > 0) {
-		givenCode = givenCode.split('3 YARD PIECE')[0].trim();
+	let tempCode = givenCode;
+	if (tempCode.indexOf('3 YARD PIECE') > 0) {
+		tempCode = tempCode.split('3 YARD PIECE')[0].trim();
 	}
-	givenCode = givenCode.replace('MDG', '');
-	givenCode = givenCode.replace('"', '');
-	givenCode = givenCode.replace(/[ ]{2,}/, ' ');
+	if (tempCode.indexOf('15 YDS') > 0) {
+		tempCode = tempCode.split('15 YDS')[0].trim();
+	}
+	tempCode = tempCode.replace(/^BOLT/, '');
+	tempCode = tempCode.replace(/\$[0-9]+(?:\.[0-9]+)\s*YD/, '');
+	tempCode = tempCode.replace(/[0-9]+\s*(?:YARDS?|YD)/, '');
+	tempCode = tempCode.replace('MDG', '');
+	tempCode = tempCode.replace('QUILT BACK', '');
+	tempCode = tempCode.replace('"', '');
+	tempCode = tempCode.replace("'", '');
+	tempCode = tempCode.replace(/[ ]{2,}/, ' ');
+	tempCode = tempCode.trim();
 
-	let prefix = 'MG';
+	let prefix = 'QB'; // Quiltbacks
 	let title = getFormattedTitle();
 	title = title.replace(/(?: - )?Marshall Dry Goods/, '');
 	let dates = getReleaseDates();
 
-	let tempName = shortenColourName(givenCode);
+	let tempName = shortenColourName(tempCode);
 
 	let collectionName = title;
 	let collectionCode = '';
@@ -61,32 +80,80 @@ function getItemObject(item) {
 	let colourCode = '';
 	let colourName = '';
 	let special = '';
+
+	for (const coll of CollectionList) {
+		if (tempCode.indexOf(coll) >= 0) {
+			let tempColl = coll;
+			let tempColour = tempCode.replace(coll, '').trim();
+
+			if (coll.indexOf('DIGITAL') >= 0) {
+				tempColl = tempColl.replace(/DIGITALLY PRINTED/, 'DIGITAL').trim();
+				tempColl = tempColl.replace(/DIGITAL$/, '').trim();
+				special = 'Digital';
+			}
+
+			collectionName = tempColl.toTitleCase();
+			let longth = 1;
+			do {
+				let regex = new RegExp(`\\b([A-Za-z]\{${longth}\}|[0-9]+)`, 'g');
+				collectionCode = tempColl.match(regex).join('').toUpperCase();
+				longth++;
+			} while (collectionCode.replace(/[0-9]/g, '').length < 4);
+
+			if (tempColour.indexOf('#') >= 0) {
+				let split = tempColour.split('#');
+				split = split.map(s => s.trim());
+				split[0] = split[0].replace(/PATTERN$/, '').trim();
+
+				colourCode = split[1];
+			}
+			else {
+				colourName = tempColour.toTitleCase();
+				//colourCode = tempColour;
+			}
+
+			break;
+		}
+	}
+
 	if (tempName.indexOf('#') >= 0) {
 		let split = tempName.split('#');
 		split = split.map(s => s.trim());
 		split[0] = split[0].replace(/PATTERN$/, '').trim();
 
-		if (split[0].indexOf('DIGITAL') >= 0) {
-			split[0] = split[0].replace(/DIGITAL$/, '').trim();
-			split[0] = split[0].replace(/DIGITALLY PRINTED/, 'DIGITAL').trim();
-			special = 'Digital';
-		}
-
-		collectionCode = split[0];
-		collectionName = split[0].toTitleCase();
 		colourCode = split[1];
 	}
-	else if (givenCode.indexOf('WHITE ON WHITE') >= 0) {
-		let temp = givenCode.replace('WHITE ON WHITE', '').trim();
-		collectionCode = temp;
-		collectionName = temp.toTitleCase();
-		colourName = 'WOW';
+	else if ((tempCode.indexOf('WHITE ON') >= 0 || tempCode.indexOf('GRAY ON') >= 0) && tempCode.indexOf('PRINT') >= 0) {
+		collectionCode = '108TOT';
+		collectionName = 'Tone on Tone Print';
+
+		colourName = tempCode.replace(/^[0-9]+/g, '');
+		colourName = colourName.replace(/PRINT$/g, '');
+		colourName = colourName.trim().toTitleCase();
+		let longth = 1;
+		do {
+			let regex = new RegExp(`\\b([A-Za-z]\{1,${longth}\})`, 'g');
+			colourCode = colourName.match(regex).join('').toUpperCase();
+			longth++;
+		} while (colourCode.replace(/[0-9]/g, '').length < 4);
 	}
-	else {
-		collectionCode = tempName;
+	else if (tempCode.indexOf('WHITE ON WHITE') >= 0 && !tempCode.indexOf('PRINT') >= 0) {
+		let temp = tempCode.replace('WHITE ON WHITE', '').trim();
+
+		collectionCode = '108WOW';
+		collectionName = 'White on White';
+		colourName = temp.replace(/^[0-9]+/g, '').toTitleCase();
+		let longth = 1;
+		do {
+			let regex = new RegExp(`\\b([A-Za-z]\{1,${longth}\})`, 'g');
+			colourCode = temp.match(regex).join('').toUpperCase();
+			longth++;
+		} while (colourCode.replace(/[0-9]/g, '').length < 4);
 	}
 
-	let purchaseCode = givenCode;
+	colourName = fixColourName(colourName);
+
+	let purchaseCode = tempCode;
 	purchaseCode = formatPurchaseCode(purchaseCode.trim());
 
 	let material = 'C100%';
@@ -122,18 +189,20 @@ function formatInformation(itemElement) {
 	let relDateString = toReleaseString(item.ReleaseDates);
 	let delDateString = toDeliveryString(item.ReleaseDates);
 
-	let tempCodeColour = ((item.ColourCode.length > 0) ? item.ColourCode + ' ' : '');
-	tempCodeColour += shortenColourName(item.ColourName);
+	let tempCodeColour = ((item.ColourCode.length > 0) ? item.ColourCode : shortenColourName(item.ColourName));
 	itemCode = formatItemCode(item.Prefix, item.CollectionCode + ' ' + tempCodeColour.toUpperCase());
+	let barCode = formatBarCode(itemCode);
+
+	if (item.ColourName.length > 0) {
+		itemCode = formatItemCode(item.Prefix, item.CollectionCode + ' ' + shortenColourName(item.ColourName));
+	}
 
 	let widthString = item.Width.Measurement + item.Width.Unit;
 	description = formatSapDescription({ 'Colour': item.ColourName, 'Pattern': item.PatternName, 'Collection': item.CollectionName, 'Special': item.SpecialNotes, 'Material': item.Material, 'Width': 'W' + widthString, 'Repeat': item.Repeat })
 
-	webName = (((item.ColourName.length > 0) ? item.ColourName + ' - ' : '') + item.PatternName);
+	webName = (((item.ColourName.length > 0) ? item.ColourName + ' - ' : '') + item.CollectionName);
 
 	webDesc = formatWebDescription({ 'Collection': item.CollectionName, 'Notes': item.SpecialNotes, 'Fibre': item.Material, 'Width': widthString, 'Release': relDateString, 'Delivery From': item.ReleaseDates.Delivery });
-
-	let barCode = formatBarCode(itemCode);
 
 	let result = { 'itemCode': itemCode, 'barCode': barCode, 'description': description, 'webName': webName, 'webDesc': webDesc, 'delDate': delDateString, 'purchaseCode': item.PurchaseCode, 'webCategory': item.CollectionName };
 	return result;
