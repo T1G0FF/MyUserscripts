f48d9cca28f482904a274c89e2a5ddfe128c33ad
Thomas <thomas.gitcommit@outlook.com>
2025-01-31 09:33:43 +1100

    Update BlankQuilting-StudioE.user.js
    Updated regex to account for items without a colour code, but still have a colour name.

--- CollectionExtractors/BlankQuilting-StudioE.user.js
+++ CollectionExtractors/BlankQuilting-StudioE.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VicText Collection Extractor - Blank Quilting / Studio E
 // @namespace    http://www.tgoff.me/
-// @version      2024.10.25.1
+// @version      2025.01.31.1
 // @description  Gets the names and codes from a Blank Quilting or Studio E Collection
 // @author       www.tgoff.me
 // @match        *://www.blankquilting.net/*
@@ -107,7 +107,7 @@ async function CollectionsToClipBoard(collection) {
 	await Notify.log(msg);
 }
 
-let blankRegEx = /([a-zA-Z]+)?([0-9]+)([a-zA-Z]+)?(?:-[ ]*(?:([0-9]+)([a-zA-Z]+)?)?)(?:[ ]*([\w\ \-\.\/]+))?/;
+let blankRegEx = /([a-zA-Z]+)?([0-9]+)([a-zA-Z]+)?(?:-[ ]*(?:([0-9]+)([a-zA-Z]+)?)?)?(?:[ ]*([\w\ \-\.\/]+))?/;
 let RegexEnum = {
 	'Prefix': 1,
 	'Collection': 2,
