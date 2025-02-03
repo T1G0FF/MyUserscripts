eae2841ccf17b28f275696938554d3d68ac586aa
Thomas <thomas.gitcommit@outlook.com>
2025-02-03 14:36:46 +1100

    Update StraightSell-CMS-Image-Upload-Presets.user.js
    Updated to move preset buttons and adjust input widths so page contents remain visible on narrower resolutions.

--- Other/StraightSell-CMS-Image-Upload-Presets.user.js
+++ Other/StraightSell-CMS-Image-Upload-Presets.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Straightsell CMS Image Upload Presets
 // @namespace    http://www.tgoff.me/
-// @version      2020.03.17.1
+// @version      2025.02.03.1
 // @description  Provides single click presets for image uploads.
 // @author       www.tgoff.me
 // @match        *://cp.straightsell.com.au/index.php?app=cm&section=docsManage*
@@ -9,15 +9,41 @@
 // @grant        none
 // ==/UserScript==
 
+var width = '750px';
+
 (function () {
 	'use strict';
 	addPresetButtons();
+	fixInputWidths();
 })();
 
 function addPresetButtons() {
-	let buttonLocationElement = document.querySelector('select#folder');
-	if (buttonLocationElement) {
-		let dest = document.querySelector('select#folder');
+	let dest = document.querySelector('select#folder');
+
+	if (dest) {
+		let cssText = `
+div.presetContainer {
+	justify-content: end;
+}
+/* Smaller than width */
+@media (max-width:${width}) {
+	div.presetContainer {
+		flex-direction: column;
+	}
+}
+/* Larger than width */
+@media (min-width:${width}) {
+
+}`;
+		addStyle(cssText);
+
+		let locParent = dest.parentElement
+		let buttonLocationElement = locParent.cloneNode();
+		buttonLocationElement.replaceChildren();
+		buttonLocationElement.classList.add('presetContainer');
+		buttonLocationElement.classList.add('widthFix');
+		locParent.insertAdjacentElement('AfterEnd', buttonLocationElement);
+
 		let unzip = document.querySelector('input#unzip');
 		let pub = document.querySelector('input[name*="publishUP"]');
 		let optim = document.querySelector('input[name*="optimiseJpegs"]');
@@ -38,7 +64,7 @@ function addPresetButtons() {
 			thumbCreate.checked = false;
 			fieldUpdate();
 		};
-		buttonLocationElement.insertAdjacentElement('AfterEnd', buttonElement);
+		buttonLocationElement.insertAdjacentElement('AfterBegin', buttonElement);
 
 		buttonElement = document.createElement('button');
 		buttonElement.innerText = 'Category Preset';
@@ -53,7 +79,7 @@ function addPresetButtons() {
 			thumbCreate.checked = false;
 			fieldUpdate();
 		};
-		buttonLocationElement.insertAdjacentElement('AfterEnd', buttonElement);
+		buttonLocationElement.insertAdjacentElement('AfterBegin', buttonElement);
 
 		buttonElement = document.createElement('button');
 		buttonElement.innerText = 'Thumbs Preset';
@@ -70,7 +96,7 @@ function addPresetButtons() {
 			thumbSize.value = '50';
 			fieldUpdate();
 		};
-		buttonLocationElement.insertAdjacentElement('AfterEnd', buttonElement);
+		buttonLocationElement.insertAdjacentElement('AfterBegin', buttonElement);
 
 		buttonElement = document.createElement('button');
 		buttonElement.innerText = 'Fullsize Preset';
@@ -85,7 +111,7 @@ function addPresetButtons() {
 			thumbCreate.checked = false;
 			fieldUpdate();
 		};
-		buttonLocationElement.insertAdjacentElement('AfterEnd', buttonElement);
+		buttonLocationElement.insertAdjacentElement('AfterBegin', buttonElement);
 	}
 }
 
@@ -98,3 +124,44 @@ function fieldUpdate() {
 		document.getElementById('thumbnailSizeUpload').disabled = true;
 	}
 }
+
+function fixInputWidths() {
+	let cssText = `
+div.widthFix {
+	width: 50%;
+}
+
+/* Smaller than width */
+@media (max-width:${width}) {
+	div.widthFix {
+		min-width: 100%;
+	}
+}
+/* Larger than width */
+@media (min-width:${width}) {
+	div.widthFix {
+		min-width: ${width};
+	}
+}`;
+	addStyle(cssText);
+
+	var inputList = document.querySelectorAll('div.input-group[style*="width:50%"], div.input-group[style*="width: 50%"]');
+	for (const element of inputList) {
+		element.style.width = '';
+		element.classList.add('widthFix');
+	}
+}
+
+function addStyle(css, disabled = false) {
+	let node = document.createElement('style');
+	node.type = 'text/css';
+	node.appendChild(document.createTextNode(css));
+	let heads = document.getElementsByTagName('head');
+	if (heads.length > 0) {
+		heads[0].appendChild(node);
+	} else {
+		document.documentElement.appendChild(node);
+	}
+	node.disabled = disabled;
+	return node;
+};
